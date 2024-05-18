53a5413195740d0df5ae768309d910a311bd2120
NJ-PRO <naaji50@gmail.com>
2024-05-19 00:16:43 +0530

    back to the NLP AI modal

--- app.py
+++ app.py
@@ -59,8 +59,8 @@ app = Flask(__name__)
 @app.route("/bot", methods=["POST"])
 def response():
     query = dict(request.form)['query']
-    # res = chat(query)
-    res = chatGen(query)
+    res = chat(query)
+    # res = chatGen(query)
     return jsonify({"response" : res})
 if __name__=="__main__":
     app.run(host="0.0.0.0",)
